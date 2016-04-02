2f8e77bd169907306f24654cd1a99cafef16cc4a
Terrence Ko <terrenceko@burnside-ac3-lan.gw.mcgill.ca>
2016-04-02 16:19:57 -0400

    Trying to push

--- server.js
+++ server.js
@@ -4,20 +4,79 @@
 var express= require('express');
 var bodyParser= require('body-parser')
 var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/1234');
-
+var http = require('http');
+// mongoose.connect('mongodb://localhost:27017');
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   // we're connected!
+// });
 var app=express();
-app.use(cors());
-// app.use(express.static('/Client'));
-app.use(bodyParser);
+var PORT = 8080;
 
+function handleRequest(request, response){
+  if(request.method === "POST") {
+    if (request.url === "/loadWorld") {
+      console.log("Sending world data");
+      response.writeHead(200,{"Content-Type": "application/JSON"})
+      var otherArray = ["item1", "item2"];
+      var otherObject = { item1: "item1val", item2: "item2val" };
+      var json = JSON.stringify({
+        anObject: otherObject,
+        anArray: otherArray,
+        another: "item"
+      });
+      response.end(json);
+    }
+    else if (request.url === "/saveWorld"){
+      console.log("Save world data");
+      response.writeHead(200,{"Content-Type": "text/plain"})
+      // response.setEncoding('utf8');
+      request.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
+      });
+      response.end('I think I managed to save' )
 
+    }
+  }
+}
 
-app.use(function(err, req, res, next) {
-  res.status(500).send('Something broke');
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
 });
-var router = express.Router();
 
 
-app.listen(8080, function () {console.log('App listening');});
+
+// app.use(express.static('/Client'));
+// app.use(bodyParser);
+//
+//
+//
+// app.use(function(err, req, res, next) {
+//   res.status(500).send('Something broke');
+// });
+// var router = express.Router();
+//
+// app.post('/test', function(req, res, next) {
+// var body = JSON.parse(req.body.data);
+// //   var queryNum = parseInt(body.num);
+// var data = body.data;
+// console.log(data)
+// console.log("HOOGA")
+// //   // get(queryNum, data, function(err, data) {
+// //   //   if (err) {
+// //   //     return next(err);
+// //   //   }
+// //     data='successful'
+// //     res.send(data);
+// //     next();
+//   });
+//
+//
+//
+// app.listen(8080, function () {console.log('App listening');});
